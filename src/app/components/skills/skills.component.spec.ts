f5960ff936db3871d69153fd16bb66c69b6b9eb3
Laura Buf <laura.buf@outlook.com>
2025-02-16 15:24:17 +0800

    updates to the last commit

--- src/app/components/skills/skills.component.spec.ts
+++ src/app/components/skills/skills.component.spec.ts
@@ -1,6 +1,7 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { SkillsComponent } from './skills.component';
+import { beforeEach, describe, expect, it } from 'vitest';
 
 describe('SkillsComponent', () => {
   let component: SkillsComponent;
